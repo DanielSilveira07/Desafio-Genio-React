7cd264c8add7460e0b9d9d6c7df60f9b75e65fd4
Daniel Silveira <danieldasilvasilveira2002@gmail.com>
2024-10-25 21:15:13 -0300

    concluido

--- src/layout/Genio.jsx
+++ src/layout/Genio.jsx
@@ -12,7 +12,7 @@ export function Genio() {
         <section className={styles.container}>
           <div className={styles.img_content}>
             <img
-                src={showGenio ? "https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/lampada-com-mago.png?raw=true" : "https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/lampada.png?raw=true"}
+                src={showGenio ? "/src/assets/lampada-com-mago.png" : "/src/assets/lampada.png"}
                 alt={showGenio ? "imagem do gênio" : "imagem da lâmpada do gênio"}
                 className={styles.image}
             />
